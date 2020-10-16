3c769f9f9ac418c208e3725de4dc5db63d86d161
RiddhiNilawar10 <72731542+RiddhiNilawar10@users.noreply.github.com>
2020-10-16 12:47:33 +0530

    Update background_subtraction.py

--- background_subtraction.py
+++ background_subtraction.py
@@ -6,27 +6,26 @@ def get_frame(cap, scaling_factor):
     # Read the current frame from the video capture object
     _, frame = cap.read()
 
-    # Resize the image
+    # Resize image
     frame = cv2.resize(frame, None, fx=scaling_factor, 
             fy=scaling_factor, interpolation=cv2.INTER_AREA)
 
     return frame
 
 if __name__=='__main__':
-    # Define the video capture object
+  
     cap = cv2.VideoCapture(0)
 
-    # Define the background subtractor object
+  
     bg_subtractor = cv2.createBackgroundSubtractorMOG2()
      
     
     history = 100
 
-    # Define the learning rate
+  
     learning_rate = 1.0/history
 
-    # Keep reading the frames from the webcam 
-    # until the user hits the 'Esc' key
+    
     while True:
         # Grab the current frame
         frame = get_frame(cap, 0.5)
@@ -37,17 +36,17 @@ if __name__=='__main__':
         # Convert grayscale image to RGB color image
         mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
-        # Display the images
+        # Display image
         cv2.imshow('Input', frame)
         cv2.imshow('Output', mask & frame)
 
-        # Check if the user hit the 'Esc' key
+ 
         c = cv2.waitKey(10)
         if c == 27:
             break
 
-    # Release the video capture object
+  
     cap.release()
     
-    # Close all the windows
+   
     cv2.destroyAllWindows()
